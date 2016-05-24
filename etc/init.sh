e2725b8cf5687f43e57040c54fb3eb83c2813f7b
Lenora22 <2005lun@mail.ru>
2016-05-24 16:21:51 +0500

    Update init.sh

--- etc/init.sh
+++ etc/init.sh
@@ -1,4 +1,3 @@
-sudo ln -s /home/box/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/etc/hello.py   /etc/gunicorn.d/test
-sudo /etc/init.d/gunicorn restart
+
