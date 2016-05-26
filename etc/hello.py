ee950ec648c911b5c62569a6eb272d19b7ecc31c
Lenora22 <2005lun@mail.ru>
2016-05-26 16:10:45 +0500

    Update hello.py

--- etc/hello.py
+++ etc/hello.py
@@ -2,7 +2,9 @@ server {
   listen 0.0.0.0:8080
   error_log /home/box/web/error2.log;
   access_log /home/box/web/access2.log;
-  location / {
-    root /home/box/web/hello.py;
+  location ^~ /hello/ {
+  proxy_pass http://127.0.0.1:8080;
+        proxy_set_header Host host;proxysetheaderX−Forwarded−Forhost;proxysetheaderX−Forwarded−Forproxy_add_x_forwarded_for;
+    }
  }
 }
